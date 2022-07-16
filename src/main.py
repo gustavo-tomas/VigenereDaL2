34395cbbd4f53c8d789904faa8f2f8f8c8cd6378
Gustavo Tomás <guga.tomas10@gmail.com>
2022-07-16 09:33:27 -0300

    added file i/o

--- src/main.py
+++ src/main.py
@@ -3,36 +3,51 @@
 # -> Cypher/Decypher
 # -> Key recovery
 
-from utils import get_file_content
+from utils import get_file_content, set_file_content
 from part1 import cypher, decypher
 from part2 import key_decypher, EN, BR
 
+CYPHER = 0
+DECYPHER = 1
+KEY_RECOVERY = 2
+
 def main():
 
-  message = get_file_content("input/message.txt")
-  key = get_file_content("input/key.txt")
-  cryptogram = get_file_content("input/cryptogram.txt")
+  action = int(input("Choose an action (0 - Cypher | 1 - Decypher | 2 - Key recovery): "))
 
-  # Part 1
-  enc_message = cypher(message, key)
-  dec_message = decypher(enc_message, key)
+  if action == CYPHER:
+    message_file = input("Choose a file with a message: ")
+    key_file = input("Choose a file with a key: ")
+    
+    message = get_file_content(message_file)
+    key = get_file_content(key_file)
+
+    cyphered_message = cypher(message, key)
+    set_file_content("output/" + message_file.split("/")[-1].strip(".txt") + "_cyphered.txt", cyphered_message)
   
-  print("PART 1 ------------------------------------------------------------")
-  print("MESSAGE:\t\t", message)
-  print("KEY:\t\t\t", key)
-  print("CRYPTOGRAM:\t\t", enc_message)
-  print("DEC_MESSAGE:\t", dec_message)
-  print("\n")
+  elif action == DECYPHER:
+    cryptogram_file = input("Choose a file with a cryptogram: ")
+    key_file = input("Choose a file with a key: ")
+
+    cryptogram = get_file_content(cryptogram_file)
+    key = get_file_content(key_file)
+
+    cyphered_message = decypher(cryptogram, key)
+    set_file_content("output/" + cryptogram_file.split("/")[-1].strip(".txt") + "_decyphered.txt", cyphered_message)
   
-  # Part 2
-  print("PART 2 ------------------------------------------------------------")
-  guess_key = key_decypher(cryptogram, EN)
-  rec_message = decypher(cryptogram, guess_key)
+  elif action == KEY_RECOVERY:
+    cryptogram_file = input("Choose a file with a cryptogram: ")
+
+    cryptogram = get_file_content(cryptogram_file)
+
+    rec_key = key_decypher(cryptogram, EN)
+    rec_message = decypher(cryptogram, rec_key)
+    set_file_content("output/" + cryptogram_file.split("/")[-1].strip(".txt") + "_recovered_key.txt", rec_key)
+    set_file_content("output/" + cryptogram_file.split("/")[-1].strip(".txt") + "_recovered_message.txt", rec_message)
   
-  print("CRYPTOGRAM:\t", cryptogram)
-  print("KEY GUESS:\t", guess_key)
-  print("REC MESSAGE:", rec_message)
-  print("\n")
+  else:
+    print("dumb lol")
+    return
 
   return
 
